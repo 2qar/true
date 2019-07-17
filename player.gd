4d848634ddf837b7de6e7e0faacdb6e3ec14e37a
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-07-17 11:33:52 -0700

    add the placeholder gang

--- player.gd
+++ player.gd
@@ -1,41 +1,48 @@
 extends KinematicBody2D
 
-var modulates = [Color(1.0, 0.0, 0.0, 1.0), Color(0.0, 0.0, 1.0, 1.0), Color(0.0, 1.0, 0.0, 1.0)]
+var sprites = [preload("res://red.png"), preload("res://blue.png"), preload("res://green.png")]
+var modulates : PoolColorArray = []
 var colors = ["red", "blue", "green"]
 export(int, "Red", "Blue", "Green") var color
+export(int, "Red", "Blue", "Green") var level_color
+
+export(int) var move_speed
+export(int) var jump_height
 
 var movement : Vector2
 onready var start : Vector2 = position
 onready var start_color : int = color
-var bg_color : Color
 
 func _ready():
-	$Sprite.modulate = modulates[color]
-	VisualServer.set_default_clear_color(modulates[2])
-	bg_color = modulates[2]
+	for sprite in sprites:
+		var data = sprite.get_data()
+		data.lock()
+		modulates.append(data.get_pixel(4, 4).to_html())
+		data.unlock()
+
+	VisualServer.set_default_clear_color(modulates[level_color])
+	$Sprite.texture = sprites[color]
 
 func _physics_process(delta):
 	movement.y += 10
 	
 	if Input.is_action_just_pressed("left"):
-		$Sprite.modulate = Color(0.0, 1.0, 0.0, 1.0)
-		color = 2
+		$Sprite.texture = sprites[color]
+		set_sprite(2)
 	if Input.is_action_just_pressed("right"):
-		$Sprite.modulate = Color(0.0, 0.0, 1.0, 1.0)
-		color = 1
+		set_sprite(1)
 
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
 		if Input.is_action_pressed("left"):
-			movement.x = -200
+			movement.x = -move_speed
 		if Input.is_action_pressed("right"):
-			movement.x = 200
+			movement.x = move_speed
 	else:
 		movement.x = 0
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		$Sprite.modulate = Color(1.0, 0.0, 0.0, 1.0)
-		color = 0
-		movement.y = -450
+		set_sprite(0)
+		movement.y = -jump_height
 
 	movement = move_and_slide(movement, Vector2(0, -1))
 	for i in get_slide_count():
@@ -43,13 +50,16 @@ func _physics_process(delta):
 		if collision.collider.name != colors[color]:
 			restart()
 
-	if $Sprite.modulate == bg_color:
+	if color == level_color:
 		restart()
+
+func set_sprite(c: int):
+	$Sprite.texture = sprites[c]
+	color = c
 
 func restart():
 	movement = Vector2()
 	position = start
-	$Sprite.modulate = modulates[start_color]
-	color = start_color
+	set_sprite(start_color)
 	Input.action_release("left")
 	Input.action_release("right")
